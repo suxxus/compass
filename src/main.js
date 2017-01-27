7f9ef0592fe8b1f739f234e0ed1423e2dde3148f
suxxus <suxxus@outlook.com>
2017-01-27 04:47:06 +0100

    refactored mainjs In a more functional approach

--- src/main.js
+++ src/main.js
@@ -1,57 +1,76 @@
  'use strict';
  (function(win) {
 
-     var $qs = win.document.querySelector.bind(document);
-     var smallDevice = 751;
-     var $compassContainer, $compassGraphic;
+     var Box = function(x) {
+         return {
+             map: function(f) {
+                 return Box(f(x));
+             },
+             fold: function(f) {
+                 return f(x);
+             }
+         };
+     };
 
-     var mouseOverEvtListener = function(e) {
+     var mouseOverEvtListener = function($compassContainer, $compassGraphic) {
 
-         if (document.documentElement.clientWidth <= smallDevice) {
-             return;
+         var smallDevice = 751;
+
+         return function(e) {
+
+             if (win.document.documentElement.clientWidth <= smallDevice) {
+                 return;
+             }
+
+             $compassGraphic.style.cssText = Box($compassContainer.getBoundingClientRect())
+                 .map(function(containerRect) {
+                     return {
+                         x: (e.pageX - containerRect.left) - containerRect.width / 2,
+                         y: (e.pageY - containerRect.top) - containerRect.height / 2
+                     }
+                 })
+                 .map(function(positions) {
+                     return Math.atan2(positions.x, positions.y)
+                 })
+                 .map(function(radians) {
+                     return Math.round((radians * (180 / Math.PI) * -1) + 180 - 45);
+                 })
+                 .fold(function(degree) {
+                     return 'transform:rotate(' + degree + 'deg); transition: transform 2s;';
+                 });
+
+             $compassContainer.className = [].slice
+                 .call($compassContainer.classList)
+                 .concat('help-item-is-selected')
+                 .join(' ');
+
          }
-
-         var containerRect = $compassContainer.getBoundingClientRect();
-         var axisX = containerRect.width / 2;
-         var axisY = containerRect.height / 2;
-         var posX = (e.pageX - containerRect.left) - axisX;
-         var posY = (e.pageY - containerRect.top) - axisY;
-         var radians = Math.atan2(posX, posY);
-         var degree = Math.round((radians * (180 / Math.PI) * -1) + 180 - 45);
-         var className = [].slice.call($compassContainer.classList)
-             .concat('help-item-is-selected').join(' ');
-
-         $compassGraphic.style.cssText = 'transform:rotate(' + degree + 'deg); transition: transform 2s;';
-         $compassContainer.className = className;
-
      };
 
-     var mouseOutListener = function() {
+     var mouseOutListener = function($compassContainer) {
 
-         var className = [].slice.call($compassContainer.classList)
-             .filter(function(item) {
-                 return item !== 'help-item-is-selected'
-             })
-             .join(' ');
-         $compassContainer.className = className;
+         var removeClassName = function(item) {
+             return item !== 'help-item-is-selected';
+         };
 
-     };
-
-     var addAnchorEventListeners = function() {
-
-         win.delegate($compassContainer, 'a', 'mouseover', mouseOverEvtListener, false);
-         win.delegate($compassContainer, 'a', 'mouseout', mouseOutListener, false);
-
+         return function() {
+             $compassContainer.className = [].slice
+                 .call($compassContainer.classList)
+                 .filter(removeClassName)
+                 .join(' ');
+         }
      };
 
      var onDomContentLoaded = function() {
 
-         $compassContainer = $qs('#compass-container');
-         $compassGraphic = $qs('#compass-container svg')
-         addAnchorEventListeners();
+         var $qs = win.document.querySelector.bind(document),
+             $compassContainer = $qs('#compass-container'),
+             $compassGraphic = $qs('#compass-container svg');
 
+         win.delegate($compassContainer, 'a', 'mouseover', mouseOverEvtListener($compassContainer, $compassGraphic), false);
+         win.delegate($compassContainer, 'a', 'mouseout', mouseOutListener($compassContainer), false);
      };
 
-     win.document.addEventListener("DOMContentLoaded", onDomContentLoaded);
+     win.document.addEventListener('DOMContentLoaded', onDomContentLoaded);
 
  }(window));
